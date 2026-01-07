49bbcf0dc0833f29c14e795ad342327359bf2e60
RyanW <wenfung118@gmail.com>
2026-01-08 02:31:08 +0800

    question 4 and 5 done

--- TPL_Lab8_9_Practice.cpp
+++ TPL_Lab8_9_Practice.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
-//This is the question 4b answer section
-void swap(int a, int b) {
+// This is the question 4b answer section
+
+// This is pass by value
+// void swap(int a, int b) {
+//     int temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+//This is pass by reference
+void swap(int &a, int &b) {
     int temp;
     temp = a;
     a = b;
     b = temp;
 }
 
-int main() {
+// C++ does not natively support pass by value-result
+void question4(){
     int value = 2, list[5] = {1, 3, 5, 7, 9};
     swap(value, list[0]);
     cout << "The result after swap(value, list[0]) are --> value: " << value << " and list[0]: "<< list[0] <<endl;
@@ -17,10 +28,50 @@ int main() {
     cout << "The result after swap(list[0], list[1]) are --> list[0]: " << list[0] << " and list[1]: "<< list[1] <<endl;
     swap(value, list[value]);
     cout << "The result after swap(value, list[0]) are --> value: " << value << " and list[value]: "<< list[value] <<endl;
-    cout << "SO in Pass by Value, the final result of the value: " << value << " and the list[] are: ";
+    cout << "SO the final result --> \n value: " << value << " and the list[] are: ";
     for (int i =0; i<5;i++){
         cout << " " << list[i];
     }
     cout << endl;
+}
+
+int qlist[2] = {1, 3}; 
+void fun_byreference (int &first, int &second) {  
+    // we can show the value here
+    cout << "After line 1: "<< first << " " << second << endl;
+    first += first;  	 	// Line 2  
+    cout << "After line 2: "<< first << " " << second << endl;
+    second += second; 	 	// Line 3  
+    cout << "After line 3: "<< first << " " << second << endl;
+    cout << "The list[0]: " << qlist[0] << " and the list[1]: " << qlist[1]<<endl;
+} 
+void fun_byvalue (int first, int second) {  
+    // we can show the value here
+    cout << "After line 1: "<< first << " " << second << endl;
+    first += first;  	 	// Line 2  
+    cout << "After line 2: "<< first << " " << second << endl;
+    second += second; 	 	// Line 3  
+    cout << "After line 3: "<< first << " " << second << endl;
+}   
+
+
+void question5(){
+     
+    cout << "The list[2] contain: " << qlist[0] << " and " <<qlist[1] << endl;
+    // fun_byvalue(list[0], list[1]); 	 // Line 1  
+    // cout << "This is the output in line 4 --> list[2] values: "<< list[0] << " " << list[1]; // Line 4  
+
+    fun_byreference(qlist[0], qlist[1]);
+    cout << qlist[0] << qlist[1];
+    cout << "This is the output in line 4 --> list[2] values: "<< qlist[0] << " " << qlist[1]; // Line 4 
+
+}  
+
+int main() {
+    
+    //question4();
+    
+    question5();
+
     return 0;
 }
